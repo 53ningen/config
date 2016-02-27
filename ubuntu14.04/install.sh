13a5aff9a6fc5f38fe9894935c06d0f25b62b1a2
53ningen <korun24276@gmail.com>
2016-02-28 05:46:44 +0900

    ubuntu14.04: install add-apt-repository & java8

--- ubuntu14.04/install.sh
+++ ubuntu14.04/install.sh
@@ -1,3 +1,14 @@
 # mariadb-server
 sudo apt-get -y install mariadb-server
 
+# add-aot-repository
+sudo apt-get -y install apt-file
+sudo apt-file update
+sudo apt-file search add-apt-repository
+sudo apt-get -y install software-properties-common
+
+# oracle jdk8
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get -y install oracle-java8-installer
+
