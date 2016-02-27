f0fb7b656945f86db8f9a60f52f6aacc0d12edb2
53ningen <korun24276@gmail.com>
2016-02-27 15:56:34 +0900

    add ubuntu14.04/install_jenkins.sh

---
+++ ubuntu14.04/install_jenkins.sh
@@ -0,0 +1,7 @@
+# ref. https://wiki.jenkins-ci.org/display/JENKINS/Installing+Jenkins+on+Ubuntu
+
+https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+
